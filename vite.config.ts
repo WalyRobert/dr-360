3234eb0a916dc9adeafecdcb56e5bd88c3c1c649
WalyRobert <121948948+WalyRobert@users.noreply.github.com>
2025-12-10 09:54:17 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
   server: {
     port: 3000,
-    host: '0.0.0.0',
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    }
   }
 });
