d640959dd14006e245c6125ff95c45b48a6b3e08
WalyRobert <121948948+WalyRobert@users.noreply.github.com>
2025-12-18 15:12:41 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
+import Video360Viewer from './components/Video360Viewer';
 
 const App: React.FC = () => {
-  return <LandingPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/player" element={<Video360Viewer />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
